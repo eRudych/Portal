598b3ebea57d1c2864d9e08faa1295a012891173
Liza <liza79682@gmail.com>
2020-02-04 16:19:53 +0200

    NewOpportunityPortalApplication

---
+++ src/main/java/task/NewOpportunityPortal/NewOpportunityPortalApplication.java
@@ -0,0 +1,13 @@
+package task.NewOpportunityPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewOpportunityPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewOpportunityPortalApplication.class, args);
+	}
+
+}
